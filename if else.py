517e48f377e0c2a7bbb6a3285ee4acd7d1e72841
champaksworld <champaksworld@users.noreply.github.com>
2021-04-09 13:58:40 +0530

    if else

---
+++ if else.py
@@ -0,0 +1,3 @@
+a=3
+b=3
+print("a=",a,"b=",b,"a<b",a<b,"a>b",a>b,"a<=b",a<=b,"a>=b",a>=b,"a>b",a>b,"a==b",a==b,"a!=b",a!=b)
---
+++ list processing.py
@@ -0,0 +1,6 @@
+l=[]
+print(l)
+l.append(10)
+print(l)
+l.append(11)
+print(l)
