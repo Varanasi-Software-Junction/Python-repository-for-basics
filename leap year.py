a82f6e9a90a408702d89f96d85ee56c4ec793388
champaksworld <champaksworld@users.noreply.github.com>
2021-04-12 18:54:12 +0530

    Update leap year.py

--- leap year.py
+++ leap year.py
@@ -3,7 +3,13 @@
 # if year is div by 4 and not div by 100 leap year
 # in all other cases not a leap year
 year=int(input("Enter the year "))
-if year % 400 ==0 or year % 4==0 and year % 100!=0:
+if year % 400 ==0:
     print("Leap Year")
 else:
-    print("Not leap year")
+    if year % 100 ==0:
+        print("Not leap year")
+    else:
+        if year % 4==0:
+            print("Leap Year")
+        else:
+            print("Not leap year")
