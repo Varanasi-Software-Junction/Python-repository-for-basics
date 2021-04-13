7455fbe1e208b2e6bca358b9e137a30bf32f423b
champaksworld <champaksworld@users.noreply.github.com>
2021-04-13 14:55:32 +0530

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,30 @@
+a=0
+"""
+a=0
+b=1
+c=1
+
+a=1
+b=1
+c=2
+
+a=1
+b=2
+c=3
+
+a=2
+b=3
+c=5
+
+
+"""
+b=1
+n=100
+print(1,"=",a)
+print(2,"=",b)
+for i in range(3,n+1):
+    c=a+b
+   # print(c / b)
+    print(i,"=",c)
+    a=b
+    b=c
