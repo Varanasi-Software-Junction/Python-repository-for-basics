ad8fefed956c1588a4e6a9f96fd1850a92b5d1b3
champaksworld <champaksworld@users.noreply.github.com>
2021-04-13 15:37:21 +0530

    Powers of 2

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,30 +1,6 @@
-a=0
-"""
-a=0
-b=1
-c=1
-
-a=1
-b=1
-c=2
-
-a=1
-b=2
-c=3
-
-a=2
-b=3
-c=5
-
-
-"""
-b=1
-n=100
-print(1,"=",a)
-print(2,"=",b)
-for i in range(3,n+1):
-    c=a+b
-   # print(c / b)
-    print(i,"=",c)
-    a=b
-    b=c
+n=10
+r=range(0,n+1)
+m=1
+for x in r:
+    print(m)
+    m=m*2
