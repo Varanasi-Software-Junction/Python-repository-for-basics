ff52ba72e60c0737bcdad5848b20eb7bcd40ca0d
champaksworld <champaksworld@users.noreply.github.com>
2021-04-16 15:48:53 +0530

    Hourglass

--- nestedloop2.py
+++ nestedloop2.py
@@ -1,19 +1,13 @@
-n=3
+n=7
+for row in reversed( range(2,n+1)):
+    for space in range(1,n-row +1):
+        print(" ",end="")
+    for col in range(1,2*row):
+        print("0",end="")
+    print()
 for row in ( range(1,n+1)):
     for space in range(1,n-row +1):
         print(" ",end="")
     for col in range(1,2*row):
         print("0",end="")
     print()
-
-for row in reversed( range(1,n)):
-    for space in range(1,n-row +1):
-        print(" ",end="")
-    for col in range(1,2*row):
-        print("0",end="")
-    print()
-    for row in reversed (range(1,n+1)):
-        for space in range(1,n-row+1):
-            print("0",end="")
-            print(" ",end=" ")
-            print()
