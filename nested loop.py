fc8f39154c943d9f49fb088ba5c66e884b716bbc
champaksworld <champaksworld@users.noreply.github.com>
2021-04-16 15:29:02 +0530

    Diamond

--- nested loop.py
+++ nested loop.py
@@ -1,7 +1,14 @@
-n=4
-for i in range(1,n+1):
-    for j in range(1,n-i+1):
+n=3
+for row in ( range(1,n+1)):
+    for space in range(1,n-row +1):
         print(" ",end="")
-    for k in range(1,i+1):
+    for col in range(1,2*row):
+        print("0",end="")
+    print()
+
+for row in reversed( range(1,n)):
+    for space in range(1,n-row +1):
+        print(" ",end="")
+    for col in range(1,2*row):
         print("0",end="")
     print()
