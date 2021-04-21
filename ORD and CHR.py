d8f3f74fb5bbcb63550ac564b2d3b55bc44b9672
champaksworld <champaksworld@users.noreply.github.com>
2021-04-21 16:02:04 +0530

    Update ORD and CHR.py

--- ORD and CHR.py
+++ ORD and CHR.py
@@ -12,5 +12,23 @@ for row in reversed(range(1,n)):
     for left in range(1,row+1):
         print(left,end="")
     print()
-for row in range(1,2*n):
-    print("0",end="")
+
+
+for row in range(2,n):
+    for left in range(1,row+1):
+        print(left,end="")
+    for space in range(1,2*n - 2*row):
+        print(" ",end="")
+    for left in range(1,row+1):
+        print(left,end="")
+    print()
+for row in range(1,n+1):
+    print(row,end="")
+for row in reversed(range(1, n )):
+     print(row, end="")
+print()
+
+
+
+
+
