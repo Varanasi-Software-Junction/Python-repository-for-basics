d551d731b594cb61a10415c6b65d10772107013d
champaksworld <champaksworld@users.noreply.github.com>
2021-04-21 16:25:45 +0530

    Alpha

--- ORD and CHR.py
+++ ORD and CHR.py
@@ -14,20 +14,6 @@ for row in reversed(range(1,n)):
     print()
 
 
-for row in range(2,n):
-    for left in range(1,row+1):
-        print(left,end="")
-    for space in range(1,2*n - 2*row):
-        print(" ",end="")
-    for left in range(1,row+1):
-        print(left,end="")
-    print()
-for row in range(1,n+1):
-    print(row,end="")
-for row in reversed(range(1, n )):
-     print(row, end="")
-print()
-
 
 
 
