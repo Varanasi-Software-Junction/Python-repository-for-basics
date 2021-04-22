51e69f2f5e785781c45e3e4f7e4ee7ede9efa48a
champaksworld <champaksworld@users.noreply.github.com>
2021-04-22 16:54:24 +0530

    Update Alpha.py

--- Alpha.py
+++ Alpha.py
@@ -1,37 +1,13 @@
-n=4
-a=ord("A")-1
+n=7
+mid=(n+1)/2
 for row in range(1,n+1):
-    ch=chr(a+row)
-    print(ch,end="")
-for row in reversed(range(1, n )):
-    ch = chr(a + row)
-    print(ch, end="")
-print()
-for row in reversed(range(1,n)):
-    for left in range(1,row+1):
-        ch = chr(a + left)
-        print(ch, end="")
-    for space in range(1,2*n - 2*row):
-        print(" ",end="")
-    for left in reversed(range(1,row+1)):
-        ch = chr(a + left)
-        print(ch,end="")
+    for col in range(1,n+1):
+        condition=  (row-col)==(-mid+1)  or (row+col)==(n+mid) or row==(mid)
+        if condition:
+            print(0,end="")
+        else:
+            print(" ",end="")
     print()
-
-
-for row in range(2,n):
-    for left in range(1,row+1):
-        print(ch,end="")
-    for space in range(1,2*n - 2*row):
-        print(" ",end="")
-    for left in range(1,row+1):
-        print(ch,end="")
-    print()
-for row in range(1,n+1):
-    print(row,end="")
-for row in reversed(range(1, n )):
-     print(ch, end="")
-print()
 
 
 
