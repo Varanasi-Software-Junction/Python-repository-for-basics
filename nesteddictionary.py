d28b1aa189a3ae08514fa280fb4555786b99dac1
champaksworld <champaksworld@users.noreply.github.com>
2021-06-23 15:14:05 +0530

    Update nesteddictionary.py

--- nesteddictionary.py
+++ nesteddictionary.py
@@ -2,14 +2,22 @@ rahulresult={"phy":70,"math":60}
 rahuldetails={"name":"Rahul","address":"LKO","result":rahulresult}
 students={1: {"name":"Abhay","address":"Varanasi","result":{"phy":50,"math":60}},2:rahuldetails}
 print(students)
-studentdetail=students.get(2)
-if studentdetail==None:
-    print("Not found")
-else:
-    print(studentdetail)
-    address=studentdetail["address"]
-    print("Address = ",address)
-    result=studentdetail["result"]
-    print(result)
-    phy=result["phy"]
-    print(phy)
+while True:
+    print("0-Exit, 1- Search by Roll No")
+    option=int(input("Option \n"))
+    if option==0:
+        break
+    if option==1:
+        rollno=int(input("Roll No \n"))
+        student=students.get(rollno)
+        print(student)
+        if student!=None:
+            while True:
+                print("0-Exit, 1-Name")
+                option1=int(input("Option = \n"))
+                if option1==0:
+                    break
+                if option1==1:
+                    name=student.get("name")
+                    print(name)
+
